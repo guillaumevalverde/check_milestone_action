e7966a20b4d82e186ba123988a03649aa1de2cb7
valverde Guillaume <guillaume.valverde@gmail.com>
2021-10-07 08:40:03 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,34 +24,13 @@ pull_request=$(jq --raw-output .pull_request "$GITHUB_EVENT_PATH")
 milestone=$(jq --raw-output .pull_request.milestone "$GITHUB_EVENT_PATH")
 number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 
-echo "pr :"
-echo "${pull_request}"
 echo "milestone :"
 echo "${milestone}"
 
-echo "get pr https://api.github.com/repos/"
-echo "${GITHUB_REPOSITORY}"
-echo "${number}"
-echo "stop"
-pr=`curl -sSL \
-    -H "Content-Type: application/json" \
-    -H "${AUTH_HEADER}" \
-    -H "${API_HEADER}" \
-    -X "GET" \
-"https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${number}"`
-
-
-echo "${pr}"
-
-echo "get milesotne"
-milestone=`echo "${pr}" | jq '.milestone'`
-
-echo "${milestone}"
-
-if [ -z "$milestone" ]; then
+if [ -n "$milestone" ]; then
   echo "milestone should be set up"
   exit 1
 else
-  echo "Ignoring action ${action}"
+  echo "milestone is set up"
   exit 0
 fi
